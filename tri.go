c211bc501ea1c04c8164f3556a02068bab857948
logan974 <33648275+logan974@users.noreply.github.com>
2023-02-28 11:28:41 +0400

    Amélioration et commentaire

--- tri.go
+++ tri.go
@@ -71,6 +71,7 @@ func tri_rapide(tab []int) []int {
 	tabg := []int{} //tab grand
 	if n > 1 {
 		pivot := tab[n-1]
+		//Création des 2 tableaux inférieur et supérieur au pivot
 		for i := 0; i < n-1; i++ {
 			if tab[i] < pivot {
 				tabp = append(tabp, tab[i])
@@ -78,8 +79,10 @@ func tri_rapide(tab []int) []int {
 				tabg = append(tabg, tab[i])
 			}
 		}
+		//Récursivité
 		tabg = tri_rapide(tabg)
 		tabp = tri_rapide(tabp)
+		//Fusion de la récursivité
 		tabp = append(tabp, pivot)
 		for k := 0; k < len(tabg); k++ {
 			tabp = append(tabp, tabg[k])
@@ -93,13 +96,15 @@ func tri_rapide(tab []int) []int {
 
 func fusion(tab1 []int, tab2 []int) []int {
 	tab := []int{}
-
-	// Voir : https://yourbasic.org/golang/append-explained/
-	check := (len(tab1) * 2) - 1
+	//Check par rapport à la taille du tableau finale
+	//check := (len(tab1) * 2) - 1
+	check := len(tab1) + len(tab2) - 1
 	indextab1 := 0
 	indextab2 := 0
 	for i := 0; i <= check; i++ {
+		//Condition : Les deux tableaux ont des valeurs restante à traiter
 		if indextab1 < len(tab1) && indextab2 < len(tab2) {
+			//Tri
 			if tab1[indextab1] < tab2[indextab2] {
 				tab = append(tab, tab1[indextab1])
 				indextab1 += 1
@@ -108,6 +113,7 @@ func fusion(tab1 []int, tab2 []int) []int {
 				indextab2 += 1
 			}
 		}
+		//Condition : Un des tableau a des valeurs restante à traiter
 		if indextab1 >= len(tab1) && indextab2 < len(tab2) {
 			tab = append(tab, tab2[indextab2])
 			indextab2 += 1
@@ -116,6 +122,7 @@ func fusion(tab1 []int, tab2 []int) []int {
 			tab = append(tab, tab1[indextab1])
 			indextab1 += 1
 		}
+		//Si plus de valeur à traiter
 		if indextab1 >= len(tab1) && indextab2 >= len(tab2) {
 			break
 		}
@@ -130,16 +137,18 @@ func tri_fusion(tab []int) []int {
 	n := len(tab)
 	tabgauche := []int{}
 	tabdroite := []int{}
+	// On vérifie que la tableau à au moins 2 élément
 	if n > 1 {
-
 		coupure := n / 2
-
+		//Coupure
 		tabgauche = append(tabgauche, tab[0:coupure]...)
 		tabdroite = append(tabdroite, tab[coupure:n]...)
+		//Récursivité
 		if len(tabgauche) > 1 || len(tabdroite) > 1 {
 			tabgauche = tri_fusion(tabgauche)
 			tabdroite = tri_fusion(tabdroite)
 		}
+		//Fusion
 		tab = fusion(tabgauche, tabdroite)
 
 	}
